9eb5913feaab1ea3acb35b6b5c7bf193591f591c
Daksh Developer <139046375+DakshDev@users.noreply.github.com>
2024-03-14 17:05:05 +0500

    Update script.js

--- script.js
+++ script.js
@@ -5,25 +5,26 @@
   let celInp = document.getElementById('cel');
   let fahInp = document.getElementById('fah');
 
-// logic
-        celInp.addEventListener('input',function(){
-            let c = this.value;
-            let f = (c * 9/5) + 32;
-            if(!Number.isInteger(f)){
-                f = f.toFixed(4);
-            }
-            fahInp.value = f;
-        });
+// cel
+        celInp.addEventListener('input',celFun);
+        function celFun(){
+              let c = this.value;
+              let f = (c * 9/5) + 32;
+              if(!Number.isInteger(f)){
+                  f = f.toFixed(4);
+              }
+              fahInp.value = f;
+        }
 
-        fahInp.addEventListener('input',function(){
-           let f = this.value;
-           let c =  (f - 32) * 5/9;
-           if(!Number.isInteger(c)){
-                c = c.toFixed(4);
-            }
-           celInp.value = c;
-        });
-
-
+// fah
+        fahInp.addEventListener('input',fahFun);
+        function fahFun(){
+             let f = this.value;
+             let c =  (f - 32) * 5/9;
+             if(!Number.isInteger(c)){
+                  c = c.toFixed(4);
+              }
+             celInp.value = c;
+        }
 
 
